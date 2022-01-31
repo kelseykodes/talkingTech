9525276c9b4692272d6562994f45fcb4d4a2375e
kperkins97 <perkins.kelsey@yahoo.com>
2022-01-31 17:26:50 -0600

    debugging controllers

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+// const { User } = require('../../models');
 const session = require('express-session');
 
 
@@ -64,4 +64,4 @@ router.post('/', (req, res) => {
     });
   });
 
-module.exports = router;
+module.exports = router});
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,17 @@
-const router = require("express").Router();
-const apiRoutes = require("./api");
-const homeRoutes = require("./homeRoutes.js");
-const dashRoutes = require("./dashRoutes.js");
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes.js');
+const dashRoutes = require('./dashRoutes.js');
+
+
+router.use('/api', apiRoutes);
+router.use('/dashboard', dashRoutes);
+router.use('/', homeRoutes);
+
+
 router.use((req, res) => {
     res.status(404).end();
 });
-router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashRoutes);
+
+
 module.exports = router;
